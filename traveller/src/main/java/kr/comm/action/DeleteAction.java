f3c9f9a729f8f6750cc005f160af6ed592e7ee0e
hongjaewon <hongjaewon@localhost>
2023-12-11 17:44:35 +0900

    main 수

---
+++ traveller/src/main/java/kr/comm/action/DeleteAction.java
@@ -0,0 +1,40 @@
+package kr.comm.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.comm.dao.CommDAO;
+import kr.comm.vo.CommVO;
+import kr.controller.Action;
+import kr.util.FileUtil;
+
+public class DeleteAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		Integer user_num = (Integer)session.getAttribute("user_num");
+		if(user_num == null) {
+			return "redirect:/member/loginForm.do";
+		}
+		
+		int comm_num = Integer.parseInt(request.getParameter("comm_num"));
+		
+		CommDAO dao = CommDAO.getInstance();
+		CommVO db_comm = dao.getComm(comm_num);
+		
+		if(user_num != db_comm.getMem_num()) {
+			//로그인한 회원번호와 작성자 회원번호가 불일치
+			return "/WEB-INF/views/common/notice.jsp";
+			
+		}
+		//로그인한 회원번호와 작성자 회원번호가 일치
+		dao.deleteComm(comm_num);
+		//파일 삭제
+		FileUtil.removeFile(request, db_comm.getFilename());
+		
+		return "redirect:/comm/list.do";
+	}
+
+}
