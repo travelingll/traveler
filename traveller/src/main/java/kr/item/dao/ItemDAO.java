f7a0873e41cfa2dc350832f75fc79259d295b387
현지니 <@hyunjini>
2023-12-12 12:12:39 +0900

    전체커밋

--- traveller/src/main/java/kr/item/dao/ItemDAO.java
+++ traveller/src/main/java/kr/item/dao/ItemDAO.java
@@ -1,6 +1,11 @@
 package kr.item.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import kr.item.vo.ItemVO;
+import kr.util.DBUtil;
 
 public class ItemDAO {
 private static ItemDAO instance = new ItemDAO();
@@ -11,6 +16,32 @@ private static ItemDAO instance = new ItemDAO();
 	
 	private ItemDAO(){};
 	
+	//관리자-상품 등록
+	public ItemVO insertItem(ItemVO item)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		
+		try {
+			//커넥션풀로부터 커넥션 객체 할당
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "INSERT INTO item ()";
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		
+		return item;
+	}
+	//관리자-상품 수정
+	//관리자-상품 삭제
+	//관리자/사용자 - 전체 상품 개수/검색 상품 개수
+	//관리자/사용자 - 전체 상품 목록/검색 상품 목록
+	//관리자/사용자 - 상품 상세
 	
 	
 	
--- traveller/src/main/java/kr/item/vo/ItemVO.java
+++ traveller/src/main/java/kr/item/vo/ItemVO.java
@@ -3,6 +3,7 @@ package kr.item.vo;
 import java.sql.Date;
 
 public class ItemVO {
+	
 	private int item_num;
 	private String item_name;
 	private String item_content;
@@ -22,6 +23,122 @@ public class ItemVO {
 	private Date reg_date;
 	private Date modifydate;
 	private int quantity;
+	
+	
+	public int getItem_num() {
+		return item_num;
+	}
+	public void setItem_num(int item_num) {
+		this.item_num = item_num;
+	}
+	public String getItem_name() {
+		return item_name;
+	}
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+	public String getItem_content() {
+		return item_content;
+	}
+	public void setItem_content(String item_content) {
+		this.item_content = item_content;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getItem_img1() {
+		return item_img1;
+	}
+	public void setItem_img1(String item_img1) {
+		this.item_img1 = item_img1;
+	}
+	public String getItem_img2() {
+		return item_img2;
+	}
+	public void setItem_img2(String item_img2) {
+		this.item_img2 = item_img2;
+	}
+	public String getItem_img3() {
+		return item_img3;
+	}
+	public void setItem_img3(String item_img3) {
+		this.item_img3 = item_img3;
+	}
+	public String getItem_img4() {
+		return item_img4;
+	}
+	public void setItem_img4(String item_img4) {
+		this.item_img4 = item_img4;
+	}
+	public String getItem_img5() {
+		return item_img5;
+	}
+	public void setItem_img5(String item_img5) {
+		this.item_img5 = item_img5;
+	}
+	public String getItem_img6() {
+		return item_img6;
+	}
+	public void setItem_img6(String item_img6) {
+		this.item_img6 = item_img6;
+	}
+	public int getItem_st1() {
+		return item_st1;
+	}
+	public void setItem_st1(int item_st1) {
+		this.item_st1 = item_st1;
+	}
+	public int getItem_st2() {
+		return item_st2;
+	}
+	public void setItem_st2(int item_st2) {
+		this.item_st2 = item_st2;
+	}
+	public int getItem_st3() {
+		return item_st3;
+	}
+	public void setItem_st3(int item_st3) {
+		this.item_st3 = item_st3;
+	}
+	public String getDate_start() {
+		return date_start;
+	}
+	public void setDate_start(String date_start) {
+		this.date_start = date_start;
+	}
+	public String getDate_end() {
+		return date_end;
+	}
+	public void setDate_end(String date_end) {
+		this.date_end = date_end;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public Date getModifydate() {
+		return modifydate;
+	}
+	public void setModifydate(Date modifydate) {
+		this.modifydate = modifydate;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 	
 	
 	
